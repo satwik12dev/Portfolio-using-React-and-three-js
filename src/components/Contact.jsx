d4602b15c8a6d15c9b77d3eaed086536fc7fafe1
satwik12dev <satwiksaxena41@gmail.com>
2025-09-12 20:08:41 +0530

    first commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,7 +32,7 @@ const Contact = () => {
 
     // JavaScript validation for required fields
     if (!form.name || !form.email || !form.message) {
-      alert("❌ Please fill in all fields before submitting.");
+      alert("Please fill in all fields before submitting.");
       return;
     }
 
@@ -52,7 +52,7 @@ const Contact = () => {
       .then(
         () => {
           setLoading(false);
-          alert("✅ Thank you. I will get back to you as soon as possible.");
+          alert("Thank you. I will get back to you as soon as possible.");
 
           setForm({
             name: "",
